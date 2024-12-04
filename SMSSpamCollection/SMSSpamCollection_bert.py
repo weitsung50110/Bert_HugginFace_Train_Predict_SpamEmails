ca6a83ab702e54e1f9310654e454ce10f1aa733f
weitsung50110 <a1233z51ab10@gmail.com>
2024-12-04 16:31:29 +0800

    SMSSpamCollection_bert cuda or cpu

--- SMSSpamCollection/SMSSpamCollection_bert.py
+++ SMSSpamCollection/SMSSpamCollection_bert.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
-import pandas as pd
+import torch
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-import torch
 from transformers import TrainingArguments, Trainer
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import EarlyStoppingCallback
+
+# 判斷是否有 CUDA，可用時使用 GPU，否則使用 CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"---Using device: {device}")
 
 # 讀取資料
 df = pd.read_csv('SMSSpamCollection', sep='\t',
@@ -22,7 +25,7 @@ model_name = "bert-base-uncased"
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
 # 初始化模型
-model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2)
+model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2).to(device)
 
 # 將資料集分成訓練集和驗證集
 X = list(df['message'])
@@ -30,18 +33,18 @@ y = list(df['label'])
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
 # 使用 tokenizer 將文字轉換為模型可接受的輸入格式
-X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512)
-X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512)
+X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512, return_tensors="pt")
+X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512, return_tensors="pt")
 
-# Create torch dataset # 定義自訂的 Dataset 類別
+# 定義自訂的 Dataset 類別
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels=None):
         self.encodings = encodings
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        if self.labels:
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        if self.labels is not None:
             item["labels"] = torch.tensor(self.labels[idx])
         return item
 
@@ -52,8 +55,7 @@ class Dataset(torch.utils.data.Dataset):
 train_dataset = Dataset(X_train_tokenized, y_train)
 val_dataset = Dataset(X_val_tokenized, y_val)
 
-# ----- 2. Fine-tune pretrained model -----#
-# Define Trainer parameters # 定義評估指標的計算函式
+# 定義評估指標的計算函式
 def compute_metrics(p):
     pred, labels = p
     pred = np.argmax(pred, axis=1)
@@ -65,19 +67,20 @@ def compute_metrics(p):
 
     return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
 
-# Define Trainer # 定義 Trainer 的訓練參數
+# 定義 Trainer 的訓練參數
 args = TrainingArguments(
     output_dir="output",
     evaluation_strategy="steps",
-    eval_steps=250, #過程中每隔多久 用驗證集進行評估1次
+    eval_steps=250,  # 過程中每隔多久用驗證集進行評估1次
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
     num_train_epochs=3,
     seed=0,
     load_best_model_at_end=True,
     logging_steps=10,  # 每 10 個步驟輸出一次訓練日誌
-    report_to="tensorboard",
+    report_to="tensorboard"
 )
+
 # 初始化 Trainer
 trainer = Trainer(
     model=model,
@@ -88,8 +91,5 @@ trainer = Trainer(
     callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
 )
 
-# Train pre-trained model # 開始訓練模型
+# 開始訓練模型
 trainer.train()
-
-#https://gist.github.com/vincenttzc/ceaa4aca25e53cb8da195f07e7d0af92
-#https://github.com/krishnaik06/Huggingfacetransformer/blob/main/Custom_Sentiment_Analysis.ipynb
